a73358c56469218117ebf3a21f7baa8662ff3e3b
Laurens Chaves Méndez <laurens004@gmail.com>
2018-06-30 21:10:22 -0600

    ConsumingActionsReducers

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {tittle: 'Javascript: The Good Parts'},
-    {tittle: 'Harry Potter'},
-    {tittle: 'The Dark Tower'},
-    {tittle: 'Eloquent Ruby'}
+    {tittle: 'Javascript: The Good Parts', pages: 101},
+    {tittle: 'Harry Potter', pages: 600},
+    {tittle: 'The Dark Tower', pages: 5},
+    {tittle: 'Eloquent Ruby', pages: 90}
   ]
 }
